a61e7b674cfd4f219ab701ee3eb12b6d9832909e
dilakasap <dilakasap@gmail.com>
2021-10-01 14:13:27 +0300

    last changes

--- src/components/Card.js
+++ src/components/Card.js
@@ -18,7 +18,7 @@ function Card({ moviesArray, openModal, deleteMovie }) {
           <div className="info">
             <div className="stars">
               {/* react-stars Library */}
-              <ReactStars color2={"black"} color1={"gray"} count={5} size={30} value={item.vote_average / 2} half={false} />
+              <ReactStars color2={"black"} color1={"gray"} count={5} size={30} value={item.vote_average / 2} half={true} edit={false} />
             </div>
             <div className="point">
               <p>{item.vote_average}</p>
